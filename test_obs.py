023541b0a62de22abe32d2cb57684a8c81ab5c84
sarawut_na <sarawut_na@dtgo.com>
2023-10-27 14:27:28 +0700

    update

--- test_obs.py
+++ test_obs.py
@@ -19,7 +19,7 @@ def main() :
     obs_credentials = dict(conf.items('OBS'))
     print(obs_credentials)
 
-    bucket_name = obs_credentials['bucketName']
+    bucket_name = obs_credentials['bucketname']
 
     # Create an instance of ObsClient.
     obs_client = ObsClient(
